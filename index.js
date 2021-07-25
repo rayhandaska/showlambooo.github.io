f66175d0b56c9d0d3ad61032cd9bfa675abc97bf
rayhandaska <rayhand2001@gmail.com>
2021-07-25 13:31:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const menu = document.querySelector(".menu");
+const navOpen= document.querySelector(".hamburger");
+const navClose= document.querySelector(".close");
+const navBar = document.querySelector(".nav");
+
+const navLeft= menu.getBoundingClientRect().left
+
+navOpen.addEventListener("click", () => {
+    if(navLeft<0) {
+        menu.classList.add("show");
+        document.body.classList.add("show");
+        navBar.classList.add("show");
+    } 
+});
+navClose.addEventListener("click", () =>{
+    if (navLeft<0){
+        menu.classList.remove("show");
+        document.body.classList.remove("show");
+        navBar.classList.remove("show"); 
+    }
+});
+
+document.getElementById('btn7').onclick = function change_image()
+{
+    document.getElementById("indo4").src="./image/sell4.png";
+}
+document.getElementById('btn8').onclick = function change_image()
+{   
+    document.getElementById("indo4").src="./image/sells4.png";
+}
+
+
